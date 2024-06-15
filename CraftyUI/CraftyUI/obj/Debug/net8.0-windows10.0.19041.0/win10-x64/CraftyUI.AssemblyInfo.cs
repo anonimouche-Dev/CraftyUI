50ba4f49a57351b39b296951b2b6d60b2c0c0f32
anonimouche-Dev <nathanael.mone@outlook.fr>
2024-06-15 18:32:40 +0200

    stable 0.71 add more storys

--- CraftyUI/CraftyUI/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/CraftyUI.AssemblyInfo.cs
+++ CraftyUI/CraftyUI/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/CraftyUI.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CraftyUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f2cc2ee2b87a430b0c928260ea664ab4e4adb8fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+efcdb987dd97c9098ee1864772b9f2af774807cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CraftyUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CraftyUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
